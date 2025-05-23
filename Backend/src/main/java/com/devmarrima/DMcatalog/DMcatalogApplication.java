9fc6474ccb5dbe912d2e55209dc9e7f3b439014f
devmarrima <marrimacelso@gmail.com>
2025-05-23 00:34:31 -0300

    Project created

---
+++ Backend/src/main/java/com/devmarrima/DMcatalog/DMcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devmarrima.DMcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMcatalogApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/devmarrima/DMcatalog/DMcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarrima.DMcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DMcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
