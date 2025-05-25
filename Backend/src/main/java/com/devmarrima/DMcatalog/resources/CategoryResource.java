29245734239c88f780f53ec7f95978732b04a8d6
devmarrima <marrimacelso@gmail.com>
2025-05-25 20:27:21 -0300

    H2 database

--- Backend/src/main/java/com/devmarrima/DMcatalog/resources/CategoryResource.java
+++ Backend/src/main/java/com/devmarrima/DMcatalog/resources/CategoryResource.java
@@ -1,24 +1,26 @@
 package com.devmarrima.DMcatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devmarrima.DMcatalog.entities.Category;
+import com.devmarrima.DMcatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Eletrônicos"));
-        list.add(new Category(2L, "Calçados"));
+        List<Category> list = categoryService.findAll() ;
         return ResponseEntity.ok().body(list);
 
     }
---
+++ Backend/src/main/java/com/devmarrima/DMcatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.devmarrima.DMcatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devmarrima.DMcatalog.entities.Category;
+import com.devmarrima.DMcatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+
+    }
+
+}
