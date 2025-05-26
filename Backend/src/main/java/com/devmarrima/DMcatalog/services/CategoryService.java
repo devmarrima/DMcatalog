30af685d4f5100497a11afd9c9be102e2c076ded
devmarrima <marrimacelso@gmail.com>
2025-05-25 22:16:33 -0300

    Database seeding

--- Backend/src/main/java/com/devmarrima/DMcatalog/services/CategoryService.java
+++ Backend/src/main/java/com/devmarrima/DMcatalog/services/CategoryService.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devmarrima.DMcatalog.entities.Category;
 import com.devmarrima.DMcatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
+
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return categoryRepository.findAll();
 
