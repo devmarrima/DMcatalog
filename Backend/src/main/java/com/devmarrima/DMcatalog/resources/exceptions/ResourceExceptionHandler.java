1c301558e9e9d7e0f75f779986e8bc4ae68b5c45
devmarrima <marrimacelso@gmail.com>
2025-06-06 00:06:36 -0300

    Find category by id, Exception handling

---
+++ Backend/src/main/java/com/devmarrima/DMcatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.devmarrima.DMcatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devmarrima.DMcatalog.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> notfound(ResourceNotFoundException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError(null, null, null, null, null);
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+}
--- Backend/src/main/java/com/devmarrima/DMcatalog/services/CategoryService.java
+++ Backend/src/main/java/com/devmarrima/DMcatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devmarrima.DMcatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devmarrima.DMcatalog.dto.CategoryDTO;
 import com.devmarrima.DMcatalog.entities.Category;
 import com.devmarrima.DMcatalog.repositories.CategoryRepository;
+import com.devmarrima.DMcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -19,8 +21,16 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
-         List<Category> list = categoryRepository.findAll();
-         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> entity = categoryRepository.findById(id);
+        Category cat = entity.orElseThrow(()->new ResourceNotFoundException("Entity not found"));
+        return new CategoryDTO(cat);
 
     }
 
